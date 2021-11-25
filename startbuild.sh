1e052cc3422c62cc82802983c03a1e01d2a6c6eb
Jun <375105540@qq.com>
2021-11-25 17:12:01 +0800

    init openssl

---
+++ startbuild.sh
@@ -0,0 +1,35 @@
+export ANDROID_NDK_ROOT="/mnt/c/other/ndk/android-ndk-r20"
+# r20 没有gcc
+# for arch in armeabi armeabi-v7a armeabi-v7a-hard arm64-v8a mips mips64 x86 x86_64
+#for arch in armeabi-v7a
+# clang 只支持 armeabi-v7a arm64-v8a x86 x86_64
+for arch in armeabi-v7a
+do
+    ./temp_clang.sh $arch 
+done
+
+# CPPFLAGS="-I$(pwd)/openssl/include" LDFLAGS="-L$(pwd)/openssl/lib" \
+
+#通过chmod +x build_all.sh和chmod +x build_curl.sh命令赋予脚本执行权限。
+#https://blog.csdn.net/byhook/article/details/52187711
+#https://blog.csdn.net/zhuyunier/article/details/79442417
+
+#有两个参数
+# armeabi-v7a
+# armeabi-v7a-hard
+# arm64-v8a
+# mips
+# mips64
+# x86
+# armeabi
+# x86_64
+#  
+# 第二个参数
+# 
+# stlport (shared)
+# stlport-static
+# stlport-shared
+# gnu (shared)
+# gnu-static
+# gnu-shared
+ 
---
+++ temp_clang.sh
@@ -0,0 +1,283 @@
+#!/usr/bin/env bash
+
+# ====================================================================
+# Sets the cross compile environment for Android
+# Based upon OpenSSL's setenv-android.sh (by TH, JW, and SM).
+#
+# Crypto++ Library is copyrighted as a compilation and (as of version 5.6.2)
+# licensed under the Boost Software License 1.0, while the individual files
+# in the compilation are all public domain.
+#
+# See http://www.cryptopp.com/wiki/Android_(Command_Line) for more details
+# ====================================================================
+
+unset IS_CROSS_COMPILE
+
+unset IS_IOS
+unset IS_ANDROID
+unset IS_ARM_EMBEDDED
+
+unset AOSP_FLAGS
+unset AOSP_SYSROOT
+unset AOSP_STL_INC
+unset AOSP_STL_LIB
+unset AOSP_BITS_INC
+
+export API=21 
+
+if [ -f /proc/cpuinfo ]; then
+	JOBS=$(grep flags /proc/cpuinfo |wc -l)
+elif [ ! -z $(which sysctl) ]; then
+	JOBS=$(sysctl -n hw.ncpu)
+else
+	JOBS=2
+fi
+
+# Set AOSP_TOOLCHAIN_SUFFIX to your preference of tools and STL library.
+#   Note: 4.9 is required for the latest architectures, like ARM64/AARCH64.
+# AOSP_TOOLCHAIN_SUFFIX=4.8
+# AOSP_TOOLCHAIN_SUFFIX=4.9
+if [ -z "$AOSP_TOOLCHAIN_SUFFIX" ]; then
+    AOSP_TOOLCHAIN_SUFFIX=4.9
+fi
+
+# Set AOSP_API to the API you want to use. 'armeabi' and 'armeabi-v7a' need
+#   API 3 (or above), 'mips' and 'x86' need API 9 (or above), etc.
+# AOSP_API="android-3"     # Android 1.5 and above
+# AOSP_API="android-4"     # Android 1.6 and above
+# AOSP_API="android-5"     # Android 2.0 and above
+# AOSP_API="android-8"     # Android 2.2 and above
+# AOSP_API="android-9"     # Android 2.3 and above
+# AOSP_API="android-14"    # Android 4.0 and above
+# AOSP_API="android-18"    # Android 4.3 and above
+# AOSP_API="android-19"    # Android 4.4 and above
+# AOSP_API="android-21"    # Android 5.0 and above
+# AOSP_API="android-23"    # Android 6.0 and above
+if [ -z "$AOSP_API" ]; then
+    AOSP_API="android-21"
+fi
+
+#####################################################################
+
+# ANDROID_NDK_ROOT should always be set by the user (even when not running this script)
+#   http://groups.google.com/group/android-ndk/browse_thread/thread/a998e139aca71d77.
+# If the user did not specify the NDK location, try and pick it up. We expect something
+#   like ANDROID_NDK_ROOT=/opt/android-ndk-r10e or ANDROID_NDK_ROOT=/usr/local/android-ndk-r10e.
+
+if [ -z "$ANDROID_NDK_ROOT" ]; then
+	echo "Please set your ANDROID_NDK_ROOT environment variable first"
+	exit 1
+fi
+
+# Error checking
+if [ ! -d "$ANDROID_NDK_ROOT/toolchains" ]; then
+    echo "ERROR: ANDROID_NDK_ROOT is not a valid path. Please set it."
+    [ "$0" = "$BASH_SOURCE" ] && exit 1 || return 1
+fi
+
+#####################################################################
+
+if [ "$#" -lt 1 ]; then
+    THE_ARCH=armv7
+else
+    THE_ARCH=$(tr [A-Z] [a-z] <<< "$1")
+fi
+
+# https://developer.android.com/ndk/guides/abis.html
+case "$THE_ARCH" in
+  armv7a|armeabi-v7a)
+    TOOLCHAIN_BASE="armv7a-linux-androideabi"
+    TOOLNAME_BASE="armv7a-linux-androideabi"
+    AOSP_ABI="armeabi-v7a"
+    AOSP_ARCH="arch-arm"
+    AOSP_FLAGS="-march=armv7-a -mthumb -mfpu=vfpv3-d16 -mfloat-abi=softfp -Wl,--fix-cortex-a8 -funwind-tables -fexceptions -frtti"
+    ;;
+  hard|armv7a-hard|armeabi-v7a-hard)
+    TOOLCHAIN_BASE="armv7a-linux-androideabi"
+    TOOLNAME_BASE="armv7a-linux-androideabi"
+    AOSP_ABI="armeabi-v7a"
+    AOSP_ARCH="arch-arm"
+    AOSP_FLAGS="-mhard-float -D_NDK_MATH_NO_SOFTFP=1 -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp -Wl,--fix-cortex-a8 -funwind-tables -fexceptions -frtti -Wl,--no-warn-mismatch -Wl,-lm_hard"
+    ;;
+  neon|armv7a-neon)
+    TOOLCHAIN_BASE="armv7a-linux-androideabi"
+    TOOLNAME_BASE="armv7a-linux-androideabi"
+    AOSP_ABI="armeabi-v7a"
+    AOSP_ARCH="arch-arm"
+    AOSP_FLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp -Wl,--fix-cortex-a8 -funwind-tables -fexceptions -frtti"
+    ;;
+  armv8|armv8a|aarch64|arm64|arm64-v8a)
+    TOOLCHAIN_BASE="aarch64-linux-android"
+    TOOLNAME_BASE="aarch64-linux-android"
+    AOSP_ABI="arm64-v8a"
+    AOSP_ARCH="arch-arm64"
+    AOSP_FLAGS="-funwind-tables -fexceptions -frtti"
+    ;;
+  x86)
+    TOOLCHAIN_BASE="x86"
+    TOOLNAME_BASE="i686-linux-android"
+    AOSP_ABI="x86"
+    AOSP_ARCH="arch-x86"
+    AOSP_FLAGS="-march=i686 -mtune=intel -mssse3 -mfpmath=sse -funwind-tables -fexceptions -frtti"
+    ;;
+  x86_64|x64)
+    TOOLCHAIN_BASE="x86_64"
+    TOOLNAME_BASE="x86_64-linux-android"
+    AOSP_ABI="x86_64"
+    AOSP_ARCH="arch-x86_64"
+    AOSP_FLAGS="-march=x86-64 -msse4.2 -mpopcnt -mtune=intel -funwind-tables -fexceptions -frtti"
+    ;;
+  *)
+    echo "ERROR: Unknown architecture $1"
+    [ "$0" = "$BASH_SOURCE" ] && exit 1 || return 1
+    ;;
+esac
+
+
+# Based on ANDROID_NDK_ROOT, try and pick up the path for the tools. We expect something
+# like /opt/android-ndk-r10e/toolchains/arm-linux-androideabi-4.7/prebuilt/linux-x86_64/bin
+# Once we locate the tools, we add it to the PATH.
+# clang :$NDK/toolchains/llvm/prebuilt/$HOST_TAG/bin
+# gcc : $NDK\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin
+
+AOSP_TOOLCHAIN_PATH=""
+for host in "linux-x86_64" "darwin-x86_64" "linux-x86" "darwin-x86" "windows" "windows-x86_64"
+do
+    
+    # if [ -d "$ANDROID_NDK_ROOT/toolchains/$TOOLCHAIN_BASE-$AOSP_TOOLCHAIN_SUFFIX/prebuilt/$host/bin" ]; then
+    #     AOSP_TOOLCHAIN_PATH="$ANDROID_NDK_ROOT/toolchains/$TOOLCHAIN_BASE-$AOSP_TOOLCHAIN_SUFFIX/prebuilt/$host/bin"
+    #     break
+    # fi
+
+    if [ -d "$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/$host/bin" ]; then
+        AOSP_TOOLCHAIN_PATH="$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/$host/bin"
+        break
+    fi
+done
+
+
+#####################################################################
+
+# Error checking
+if [ ! -d "$ANDROID_NDK_ROOT/platforms/$AOSP_API" ]; then
+    echo "ERROR: AOSP_API is not valid. Does the NDK support the API? Please edit this script."
+    [ "$0" = "$BASH_SOURCE" ] && exit 1 || return 1
+elif [ ! -d "$ANDROID_NDK_ROOT/platforms/$AOSP_API/$AOSP_ARCH" ]; then
+    echo "ERROR: AOSP_ARCH is not valid. Does the NDK support the architecture? Please edit this script."
+    [ "$0" = "$BASH_SOURCE" ] && exit 1 || return 1
+fi
+
+# Android SYSROOT. It will be used on the command line with --sysroot
+#   http://android.googlesource.com/platform/ndk/+/ics-mr0/docs/STANDALONE-TOOLCHAIN.html
+# gcc ndk\platforms\android-21\arch-arm64
+# clang $NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot
+export AOSP_SYSROOT="$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/$TOOLNAME_BASE/sysroot"
+
+# TODO: export for the previous GNUmakefile-cross. These can go away eventually.
+export ANDROID_SYSROOT=$AOSP_SYSROOT
+
+
+#####################################################################
+
+# Android STL. We support GNU, LLVM and STLport out of the box.
+
+# if [ "$#" -lt 2 ]; then
+#     THE_STL=stlport-shared
+# else
+#     THE_STL=$(tr [A-Z] [a-z] <<< "$2")
+# fi
+
+# case "$THE_STL" in
+#   stlport-static)
+#     AOSP_STL_INC="$ANDROID_NDK_ROOT/sources/cxx-stl/stlport/stlport/"
+#     AOSP_STL_LIB="$ANDROID_NDK_ROOT/sources/cxx-stl/stlport/libs/$AOSP_ABI/libstlport_static.a"
+#     ;;
+#   stlport|stlport-shared)
+#     AOSP_STL_INC="$ANDROID_NDK_ROOT/sources/cxx-stl/stlport/stlport/"
+#     AOSP_STL_LIB="$ANDROID_NDK_ROOT/sources/cxx-stl/stlport/libs/$AOSP_ABI/libstlport_shared.so"
+#     ;;
+#   gabi++-static|gnu-static)
+#     AOSP_STL_INC="$ANDROID_NDK_ROOT/sources/cxx-stl/gnu-libstdc++/$AOSP_TOOLCHAIN_SUFFIX/include"
+#     AOSP_BITS_INC="$ANDROID_NDK_ROOT/sources/cxx-stl/gnu-libstdc++/$AOSP_TOOLCHAIN_SUFFIX/libs/$AOSP_ABI/include"
+#     AOSP_STL_LIB="$ANDROID_NDK_ROOT/sources/cxx-stl/gnu-libstdc++/$AOSP_TOOLCHAIN_SUFFIX/libs/$AOSP_ABI/libgnustl_static.a"
+#     ;;
+#   gnu|gabi++|gnu-shared|gabi++-shared)
+#     AOSP_STL_INC="$ANDROID_NDK_ROOT/sources/cxx-stl/gnu-libstdc++/$AOSP_TOOLCHAIN_SUFFIX/include"
+#     AOSP_BITS_INC="$ANDROID_NDK_ROOT/sources/cxx-stl/gnu-libstdc++/$AOSP_TOOLCHAIN_SUFFIX/libs/$AOSP_ABI/include"
+#     AOSP_STL_LIB="$ANDROID_NDK_ROOT/sources/cxx-stl/gnu-libstdc++/$AOSP_TOOLCHAIN_SUFFIX/libs/$AOSP_ABI/libgnustl_shared.so"
+#     ;;
+#   llvm-static)
+#     AOSP_STL_INC="$ANDROID_NDK_ROOT/sources/cxx-stl/llvm-libc++/libcxx/include"
+#     AOSP_STL_LIB="$ANDROID_NDK_ROOT/sources/cxx-stl/llvm-libc++/libs/$AOSP_ABI/libc++_static.a"
+#     ;;
+#   llvm|llvm-shared)
+#     AOSP_STL_INC="$ANDROID_NDK_ROOT/sources/cxx-stl/llvm-libc++/libcxx/include"
+#     AOSP_STL_LIB="$ANDROID_NDK_ROOT/sources/cxx-stl/llvm-libc++/libs/$AOSP_ABI/libc++_shared.so"
+#     ;;
+#   *)
+#     echo "ERROR: Unknown STL library $2"
+#     [ "$0" = "$BASH_SOURCE" ] && exit 1 || return 1
+# esac
+
+
+#####################################################################
+
+#CC=$TOOLCHAIN/bin/aarch64-linux-android$API-clang
+#CXX=$TOOLCHAIN/bin/aarch64-linux-android$API-clang++
+
+# export CPP="$AOSP_TOOLCHAIN_PATH/$TOOLNAME_BASE-cpp --sysroot=$AOSP_SYSROOT"
+# export CC="$AOSP_TOOLCHAIN_PATH/$TOOLNAME_BASE-gcc --sysroot=$AOSP_SYSROOT"
+# export CXX="$AOSP_TOOLCHAIN_PATH/$TOOLNAME_BASE-g++ --sysroot=$AOSP_SYSROOT"
+
+
+# export CPP="$AOSP_TOOLCHAIN_PATH/$TOOLNAME_BASE$API-cpp --sysroot=$AOSP_SYSROOT"
+export CC="$AOSP_TOOLCHAIN_PATH/$TOOLNAME_BASE$API-clang --sysroot=$AOSP_SYSROOT"
+export CXX="$AOSP_TOOLCHAIN_PATH/$TOOLNAME_BASE$API-clang++ --sysroot=$AOSP_SYSROOT"
+
+#####################################################################
+
+export CFLAGS=-isysroot
+export PREFIX=$(pwd)/android-lib
+export CC="$AOSP_TOOLCHAIN_PATH/$TOOLNAME_BASE$API-clang --sysroot=$AOSP_SYSROOT"
+
+
+VERBOSE=1
+if [ ! -z "$VERBOSE" ] && [ "$VERBOSE" != "0" ]; then
+  echo "job:"$JOBS
+  echo "API:"$API
+  echo "ANDROID_NDK_ROOT: $ANDROID_NDK_ROOT"
+  echo "AOSP_TOOLCHAIN_PATH: $AOSP_TOOLCHAIN_PATH"
+  echo "AOSP_ABI: $AOSP_ABI"  
+  echo "AOSP_API: $AOSP_API"
+  echo "CC: $CC"
+  echo "AOSP_SYSROOT: $AOSP_SYSROOT"
+  echo "AOSP_FLAGS: $AOSP_FLAGS"
+  # echo "AOSP_STL_INC: $AOSP_STL_INC"
+  # echo "AOSP_STL_LIB: $AOSP_STL_LIB"
+
+fi
+
+cd  openssl
+## 最终成MakeFile
+ ./config no-asm shared no-cast no-idea no-camellia
+
+# 如何支持https 需要先交叉编译https
+
+make -j$JOBS
+
+make install
+
+# CPPFLAGS="-I$(pwd)/openssl/include" LDFLAGS="-L$(pwd)/openssl/lib"
+
+
+# # ./Configure android no-asm no-shared no-cast no-idea no-camellia no-whirpool
+
+# ./configure \
+#     --prefix=$PREFIX \
+#     --enable-static \
+#     --enable-shared \
+#     --host=$TOOLNAME_BASE\
+#     --with-ssl=$(pwd)/openssl \
+#     --without-zlib
+
+[ "$0" = "$BASH_SOURCE" ] && exit 0 || return 0cd .
