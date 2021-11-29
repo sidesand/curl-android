250681a01332ece4169d839dd2ff13881cfd44ab
Jun <375105540@qq.com>
2021-11-29 14:01:14 +0800

    fix shell 8: openssl

--- start_build_openssl.sh
+++ start_build_openssl.sh
@@ -15,10 +15,10 @@ cd  openssl
 
 #here are android-arm, android-arm64, android-mips, android-mip64, android-x86 and android-x86_64 (*MIPS targets are no longer supported with NDK R20+).
 
-for arch in android-arm android-arm64 android-x86 android-x86_64
+for  in  android-x86  
 do
     PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
-    ./Configure  --libdir=$openssl_lib no-asm shared no-cast no-idea no-camellia no-comp -D__ANDROID_API__=21 --prefix=$opensslDir  --openssldir=$opensslDir
+    ./Configure $arch --libdir=$openssl_lib no-asm shared no-cast no-idea no-camellia no-comp -D__ANDROID_API__=21 --prefix=$opensslDir  --openssldir=$opensslDir
     make
     make install
 done
